5f2559604df16d2cc7ec399f25ef956ba76c4130
unknown <github@mmmad.de>
2020-07-27 16:22:50 +0200

    Refactor, move layout definition to extra script

--- dash_frontend.py
+++ dash_frontend.py
@@ -15,6 +15,7 @@ from utils import queries
 from utils import helpers
 from utils import map_traces
 from utils import timeline_chart
+from utils import dash_elements
 from utils.filter_by_radius import filter_by_radius
 from utils.get_outline_coords import get_outline_coords
 
@@ -53,7 +54,6 @@ if CLEAR_CACHE_ON_STARTUP:
 # Note: using cache.cached instead of cache.memoize
 # yields "RuntimeError: Working outside of request context."
 
-
 @cache.memoize(unless=DISABLE_CACHE)
 def get_query_api():
     url = CONFIG["influx_url"]
@@ -84,212 +84,18 @@ def get_map_traces(map_data):
 # ================
 map_data = get_map_data()  # map_data is a GeoDataFrame
 map_data, traces = get_map_traces(map_data)  # traces for main map
-chart = timeline_chart.TimelineChartWindow(TRENDWINDOW, load_timeseries)
-
-# SET UP DASH ELEMENTS
-# ======================
-
-# dcc Storage
-storage = [
-    dcc.Store(id='clientside_callback_storage', storage_type='memory'),
-    dcc.Store(id='nominatim_storage', storage_type='memory'),
-    dcc.Store(id='urlbar_storage', storage_type='memory'),
-    dcc.Store(id='highlight_polygon', storage_type='memory'),
-    dcc.Store(id='latlon_local_storage', storage_type='local'),
-]
-# Title
-title = html.H1(
-    children='EveryoneCounts',
-    style={
-        'textAlign': 'center',
-        'color': "#333",
-        'fontFamily': 'Arial, Helvetica, sans-serif'
-    }
-)
-
-config_plots = dict(
-    locale="de-DE",
-    displaylogo=False,
-    modeBarButtonsToRemove=['lasso2d',
-                            'toggleSpikelines',
-                            'toggleHover',
-                            'select2d',
-                            'autoScale2d',
-                            'resetScale2d',
-                            'resetViewMapbox'],
-    displayModeBar=True,
-    responsive=True
-)
-
-#  Dash Map
-main_map_name = "Messpunkte"
-mainmap = dcc.Graph(
-    id='map',
-    config=config_plots,
-    figure={
-        'data': traces["stations"],
-        'layout': dict(
-            autosize=True,
-            hovermode='closest',
-            showlegend=False,
-            legend_title_text='Datenquelle',
-            legend=dict(
-                x=0.5,
-                y=1,
-                traceorder="normal",
-                font=dict(
-                    family="sans-serif",
-                    size=14,
-                    color="black"
-                ),
-                bgcolor="#fff",
-                bgopacity=0.3,
-                bordercolor="#eee",
-                borderwidth=1
-            ),
-            # height=400,
-            margin=dict(l=0, r=0, t=0, b=0),
-            mapbox=dict(
-                style="carto-positron",
-                # open-street-map,
-                # white-bg,
-                # carto-positron,
-                # carto-darkmatter,
-                # stamen-terrain,
-                # stamen-toner,
-                # stamen-watercolor
-                bearing=0,
-                center=dict(
-                    lat=default_lat,
-                    lon=default_lon
-                ),
-                pitch=0,
-                zoom=6,
-            )
-        ),
-    },
-)
-# LINE CHART
-
-chartDiv = html.Div(id="chart-container", style={'display': 'none'}, children=[
-    html.Button(id="chart-close", children=" × "),
-    dcc.Loading(
-        id="timeline-chart",
-        type="circle",
-        children=[
-            dcc.Checklist(id="timeline-avg-check", value=[])
-            # This checklist  needs to be in the layout because
-            # a callback is bound to it. Otherwise, Dash 1.12 will throw errors
-            # This is an issue even when using app.validation_layout or
-            # suppress_callback_exceptions=True, as suggested in the docs
-            # Don't trust the documentation in this case.
-        ]
-    ),
-])
-
-# LOOKUP BOX
-SLIDER_MAX = 120
-lookup_span_default = "?"
-location_lookup_div = html.Div(className="", children=[
-    html.H3("Mittelpunkt bestimmen:"),
-    html.Div(id="search-container", children=[
-        dcc.Input(id="nominatim_lookup_edit", type="text", placeholder="", debounce=False),
-        html.Button(id='nominatim_lookup_button', n_clicks=0, children='Suchen'),
-    ]),
-    html.Button(id='geojs_lookup_button', n_clicks=0, children='Automatisch bestimmen'),
-    html.Button(id='mapposition_lookup_button', n_clicks=0, children='Kartenmittelpunkt verwenden'),
-    html.H3("Umkreis:"),
-    dcc.Slider(
-        id='radiusslider',
-        min=5,
-        max=SLIDER_MAX,
-        step=5,
-        value=60,
-        tooltip=dict(
-            always_visible=False,
-            placement="top"
-        ),
-        marks={20 * x: str(20 * x) + 'km' for x in range(SLIDER_MAX // 20 + 1)}
-    )
-])
-
 map_data["landkreis_label"] = map_data.apply(lambda x: x["landkreis"] + " " + str(x["districtType"]), 1)
-landkreis_options = [{'label': x, 'value': x} for x in sorted(map_data["landkreis_label"].unique())]
-bundesland_options = [{'label': x, 'value': x} for x in sorted(map_data["bundesland"].unique())]
-region_container = html.Div(id="region_container", className="container", children=[
-    dcc.Tabs(id='region_tabs', className="", value='tab-umkreis', children=[
-        dcc.Tab(label='Umkreis', value='tab-umkreis', children=[
-            location_lookup_div
-        ]),
-        dcc.Tab(label='Landkreis', value='tab-landkreis', children=[
-            html.H3("Wähle einen Landkreis:"),
-            dcc.Dropdown(
-                id='landkreis_dropdown',
-                options=landkreis_options,
-                value=landkreis_options[0]["value"],
-                clearable=False
-            ),
-            html.P("Hinweis: Nur Landkreise mit Datenpunkten können ausgewählt werden!"),
-        ]),
-        dcc.Tab(label='Bundesland', value='tab-bundesland', children=[
-            html.H3("Wähle ein Bundesland:"),
-            dcc.Dropdown(
-                id='bundesland_dropdown',
-                options=bundesland_options,
-                value=bundesland_options[0]["value"],
-                clearable=False
-            ),
-        ]),
-    ])
-])
+CHART = timeline_chart.TimelineChartWindow(TRENDWINDOW, load_timeseries)
 
-trend_container = html.Div(id="trend_container", className="container", children=[
-    html.Div(children=[
-        html.H3(f"{TRENDWINDOW}-Tage-Trend im gewählten Bereich:"),
-        html.P(id="mean_trend_p", style={}, children=[
-            html.Span(id="mean_trend_span", children="")
-        ]),
-    ]),
-    html.Div(children=[
-        html.H3("Detailgrad"),
-        dcc.RadioItems(
-            id="detail_radio",
-            options=[
-                {'label': 'Messstationen', 'value': 'stations'},
-                {'label': 'Landkreise', 'value': 'landkreis'},
-                {'label': 'Bundesländer', 'value': 'bundesland'}
-            ],
-            value='stations',
-            labelStyle={'display': 'inline-block'}
-        ),
-    ]),
-
-    html.P(id="location_p", children=[
-        html.Span(children="Region: ", style={"fontWeight": "bold"}),
-        html.Span(id="location_text", children=lookup_span_default),
-        " ",
-        html.A(children="(Ändern)", style={"textDecoration": "underline"}),
-    ]),
-    dcc.Checklist(
-        id="trace_visibility_checklist",
-        options=[{'label': helpers.measurementtitles[key], 'value': key} for key in helpers.measurementtitles],
-        value=['hystreet', 'webcam-customvision', 'bikes'],
-        labelStyle={'display': 'block'}
-    ),
-    html.P(html.A(id="permalink", children="Permalink", href="xyz")),
-])
-
-layout = [
+# SET UP DASH LAYOUT
+# ======================
+app.layout = html.Div(id="dash-layout", children=[
     dcc.Location(id='url', refresh=False),
-    *storage,
-    title,
-    mainmap,
-    trend_container,
-    region_container,
-    chartDiv
-]
-app.layout = html.Div(id="dash-layout", children=layout)
-
+    *dash_elements.storage(),
+    dash_elements.mainmap(),
+    dash_elements.main_controls(map_data, TRENDWINDOW),
+    dash_elements.timeline_chart()
+])
 
 # CALLBACK FUNCTIONS
 # ==================
@@ -337,8 +143,8 @@ def display_click_data(clickData, avg_checkbox, detail_radio):
         return dash.no_update
     prop_ids = helpers.dash_callback_get_prop_ids(ctx)
     if clickData is not None or "timeline-avg-check" in prop_ids:
-        if chart.update_figure(detail_radio, clickData, map_data, avg):
-            return [chart.get_timeline_window()]
+        if CHART.update_figure(detail_radio, clickData, map_data, avg):
+            return [CHART.get_timeline_window()]
     return dash.no_update
 
 
---
+++ utils/dash_elements.py
@@ -0,0 +1,198 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from utils import helpers
+
+SLIDER_MAX = 130
+
+
+def main_controls(map_data, TRENDWINDOW):
+    landkreis_options = [{'label': x, 'value': x} for x in sorted(map_data["landkreis_label"].unique())]
+    bundesland_options = [{'label': x, 'value': x} for x in sorted(map_data["bundesland"].unique())]
+
+    return html.Div(id="main_controls", children=[
+        html.H1(
+            children='EveryoneCounts',
+            style={
+                'textAlign': 'center',
+                'color': "#333",
+                'fontFamily': 'Arial, Helvetica, sans-serif'
+            }
+        ),
+        html.Div(id="trend_container", className="container", children=[
+            html.Div(children=[
+                html.H3(f"{TRENDWINDOW}-Tage-Trend im gewählten Bereich:"),
+                html.P(id="mean_trend_p", style={}, children=[
+                    html.Span(id="mean_trend_span", children="")
+                ]),
+            ]),
+            html.Div(children=[
+                html.H3("Detailgrad"),
+                dcc.RadioItems(
+                    id="detail_radio",
+                    options=[
+                        {'label': 'Messstationen', 'value': 'stations'},
+                        {'label': 'Landkreise', 'value': 'landkreis'},
+                        {'label': 'Bundesländer', 'value': 'bundesland'}
+                    ],
+                    value='stations',
+                    labelStyle={'display': 'inline-block'}
+                ),
+            ]),
+
+            html.P(id="location_p", children=[
+                html.Span(children="Region: ", style={"fontWeight": "bold"}),
+                html.Span(id="location_text", children="?"),
+                " ",
+                html.A(children="(Ändern)", style={"textDecoration": "underline"}),
+            ]),
+            dcc.Checklist(
+                id="trace_visibility_checklist",
+                options=[{'label': helpers.measurementtitles[key], 'value': key} for key in helpers.measurementtitles],
+                value=['hystreet', 'webcam-customvision', 'bikes'],
+                labelStyle={'display': 'block'}
+            ),
+            html.P(html.A(id="permalink", children="Permalink", href="xyz")),
+        ]),
+        html.Div(id="region_container", className="container", children=[
+            dcc.Tabs(id='region_tabs', className="", value='tab-umkreis', children=[
+                dcc.Tab(label='Umkreis', value='tab-umkreis', children=[
+                    html.Div(id="radius_search_tab", children=[
+                        html.H3("Mittelpunkt bestimmen:"),
+                        html.Div(id="search-container", children=[
+                            dcc.Input(id="nominatim_lookup_edit", type="text", placeholder="", debounce=False),
+                            html.Button(id='nominatim_lookup_button', n_clicks=0, children='Suchen'),
+                        ]),
+                        html.Button(id='geojs_lookup_button', n_clicks=0, children='Automatisch bestimmen'),
+                        html.Button(id='mapposition_lookup_button', n_clicks=0, children='Kartenmittelpunkt verwenden'),
+                        html.H3("Umkreis:"),
+                        dcc.Slider(
+                            id='radiusslider',
+                            min=5,
+                            max=SLIDER_MAX,
+                            step=5,
+                            value=60,
+                            tooltip=dict(
+                                always_visible=False,
+                                placement="top"
+                            ),
+                            marks={20 * x: str(20 * x) + 'km' for x in range(SLIDER_MAX // 20 + 1)}
+                        )
+                    ]),
+                ]),
+                dcc.Tab(label='Landkreis', value='tab-landkreis', children=[
+                    html.H3("Wähle einen Landkreis:"),
+                    dcc.Dropdown(
+                        id='landkreis_dropdown',
+                        options=landkreis_options,
+                        value=landkreis_options[0]["value"],
+                        clearable=False
+                    ),
+                    html.P("Hinweis: Nur Landkreise mit Datenpunkten können ausgewählt werden!"),
+                ]),
+                dcc.Tab(label='Bundesland', value='tab-bundesland', children=[
+                    html.H3("Wähle ein Bundesland:"),
+                    dcc.Dropdown(
+                        id='bundesland_dropdown',
+                        options=bundesland_options,
+                        value=bundesland_options[0]["value"],
+                        clearable=False
+                    ),
+                ]),
+            ])
+        ])
+    ])
+
+
+def mainmap():
+    default_lat = 50
+    default_lon = 10
+    config_plots = dict(
+        locale="de-DE",
+        displaylogo=False,
+        modeBarButtonsToRemove=['lasso2d',
+                                'toggleSpikelines',
+                                'toggleHover',
+                                'select2d',
+                                'autoScale2d',
+                                'resetScale2d',
+                                'resetViewMapbox'],
+        displayModeBar=True,
+        responsive=True
+    )
+
+    #  Dash Map
+    return dcc.Graph(
+        id='map',
+        config=config_plots,
+        figure={
+            'data': [],
+            'layout': dict(
+                autosize=True,
+                hovermode='closest',
+                showlegend=False,
+                legend_title_text='Datenquelle',
+                legend=dict(
+                    x=0.5,
+                    y=1,
+                    traceorder="normal",
+                    font=dict(
+                        family="sans-serif",
+                        size=14,
+                        color="black"
+                    ),
+                    bgcolor="#fff",
+                    bgopacity=0.3,
+                    bordercolor="#eee",
+                    borderwidth=1
+                ),
+                # height=400,
+                margin=dict(l=0, r=0, t=0, b=0),
+                mapbox=dict(
+                    style="carto-positron",
+                    # open-street-map,
+                    # white-bg,
+                    # carto-positron,
+                    # carto-darkmatter,
+                    # stamen-terrain,
+                    # stamen-toner,
+                    # stamen-watercolor
+                    bearing=0,
+                    center=dict(
+                        lat=default_lat,
+                        lon=default_lon
+                    ),
+                    pitch=0,
+                    zoom=6,
+                )
+            ),
+        },
+    )
+
+
+def storage():
+    # dcc Storage
+    return [
+        dcc.Store(id='clientside_callback_storage', storage_type='memory'),
+        dcc.Store(id='nominatim_storage', storage_type='memory'),
+        dcc.Store(id='urlbar_storage', storage_type='memory'),
+        dcc.Store(id='highlight_polygon', storage_type='memory'),
+        dcc.Store(id='latlon_local_storage', storage_type='local'),
+    ]
+
+
+def timeline_chart():
+    return html.Div(id="chart-container", style={'display': 'none'}, children=[
+        html.Button(id="chart-close", children=" × "),
+        dcc.Loading(
+            id="timeline-chart",
+            type="circle",
+            children=[
+                dcc.Checklist(id="timeline-avg-check", value=[])
+                # This checklist  needs to be in the layout because
+                # a callback is bound to it. Otherwise, Dash 1.12 will throw errors
+                # This is an issue even when using app.validation_layout or
+                # suppress_callback_exceptions=True, as suggested in the docs
+                # Don't trust the documentation in this case.
+            ]
+        ),
+    ])

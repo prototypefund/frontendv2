c750ea6dd31abdb7c22b0e996cd2274d16ef8fe1
Michael Adams <github@mmmad.de>
2020-08-06 15:33:05 +0200

    Improve update-and-restart script, don't delete everything when there is no cache folder yet

--- update-and-restart.sh
+++ update-and-restart.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git pull origin master
 secrethub inject -i config.json.tpl -o config.json --identity-provider=aws --force
-cd cache
-rm *
+rm cache/*
 sudo systemctl restart rundashboard.service
